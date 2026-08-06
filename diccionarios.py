fed300ceafdf8d9e5b0998062b7647d16bdd134a
WitzhiD04 <danielguizapro@gmail.com>
2026-08-06 16:22:01 -0500

    Diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,36 @@
+
+# Crear el diccionario
+
+Dict = {"key1": 1, "key2": "2", "key3": [3, 3, 3], "key4": (4, 4, 4), ('key5'): 5, (0, 1): 6}
+print(Dict)
+
+
+
+print(Dict[(0, 1)])
+
+print(Dict.keys())
+
+print(Dict.values())
+
+Dict["Graduation"] = "2007" #del() para borrar
+
+print(Dict)
+
+
+print("--------------------------- ACA COMIENZA EL EJERCICIO------------------------")
+
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+print(soundtrack_dic )
+
+print(soundtrack_dic.keys())
+
+print(soundtrack_dic.values())
+
+album_sales_dict = {"Back in Black":50, "The Bodyguard":50, "Thriller":65}
+print(album_sales_dict)
+
+print(album_sales_dict["Thriller"])
+
+print(album_sales_dict.keys())
+
+print(album_sales_dict.values())
